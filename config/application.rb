256dcb14b966237c91418b8b94bf33d49ce92514
Junyu Wang <junyuw@berkeley.edu>
2014-12-04 01:55:07 -0800

    heroku

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,8 @@ Bundler.require(*Rails.groups)
 module RailsDevise
   class Application < Rails::Application
 
-    config.assets.initialize_on_precompile = false
+    #config.assets.initialize_on_precompile = false
+    config.assets.precompile += %w( vendor/modernizr )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
