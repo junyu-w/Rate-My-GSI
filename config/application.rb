42e8db06344b29c1fe2301b9f88fe2d711141f77
Junyu Wang <junyuw@berkeley.edu>
2014-12-04 02:09:05 -0800

    give up on heroku, try ngrok then

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module RailsDevise
   class Application < Rails::Application
 
     #config.assets.initialize_on_precompile = false
-    config.assets.precompile += %w( vendor/modernizr )
+    #config.assets.precompile += %w( vendor/modernizr )
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
