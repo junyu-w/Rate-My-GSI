6192b5397d671b9e19c7e5d86ccc0485d172f1fd
Junyu Wang <junyuw@berkeley.edu>
2014-12-03 22:57:46 -0800

    All Finished

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141204044141) do
+ActiveRecord::Schema.define(version: 20141204060048) do
 
   create_table "comments", force: true do |t|
     t.string   "title"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20141204044141) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "average_rating"
+    t.string   "img_url"
   end
 
   create_table "replies", force: true do |t|
