38e2dbf5cd270d856af49060b0ccde96c9f6443c
Junyu Wang <junyuw@berkeley.edu>
2014-12-04 20:52:47 -0800

    heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
+#user = CreateAdminService.new.call
+#puts 'CREATED ADMIN USER: ' << user.email
 
 %w(Sam).each do |name|
   Gsi.create! name: name, description: "Hi! I'm Sam, a student studying EECS and education here at Berkeley. As an avid learner (I'm really into edX and online learning) and an enthusiastic teacher (TA'ing for CS61AS right now!) I am super excited for you guys to learn about one of the most powerful frameworks around. In my spare time I enjoy biking, bouldering, and learning new things. This course is going to be awesome, and I hope you're ready to learn a lot!", course: "Ruby on Rails", average_rating: 0, img_url:"../../assets/sam_lau.jpeg"
