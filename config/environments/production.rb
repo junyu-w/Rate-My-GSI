78e8c821de83a208c48abfe9968bcb4f9eed6350
Junyu Wang <junyuw@berkeley.edu>
2014-12-04 22:46:50 -0800

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,14 +20,17 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  
+  #for heroku
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
   # Generate digests for assets URLs.
   config.assets.digest = true
